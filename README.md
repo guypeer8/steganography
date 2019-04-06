8102a123e4c9bc6743f67f717f974bb0bc558261
Guy Peer <guy.peer@naturalint.com>
2019-04-06 12:57:35 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Steganography
+##### Steganography is the art of implanting content in a file, by inserting the underline data in the parent file's least significant bits.
+
+##### Usage:
+```python
+python3 steganography.py --encode "I know someday you'll have a beautiful life, I know you'll be the sun in somebody else's skys, but whyy! whyy! whyyy... can't it be, oh can't it be mineeeeeee... ahhh"
+# returns 166, which is the encoded text size we need to pass when decoding
+ 
+python3 steganography.py --decode 166 
+# returns the underline text: "I know someday you'll have a beautiful life, I know you'll be the sun in somebody else's skys, but whyy! whyy! whyyy... can't it be, oh can't it be mineeeeeee... ahhh"
+
+python3 steganography.py --diff
+# returns the dictionary {'pixels': 157920, 'diff': 40} - indicating how far are the 2 images from each other (at most 3 for each tgb cell portion)
+```
---
+++ steganography.py
@@ -0,0 +1,115 @@
+from os.path import splitext
+from math import sqrt, ceil
+from PIL import Image
+from sys import argv
+
+FILE = 'ocean.jpg'
+STEG_FILE = 'steganographic.png'
+
+def pad_zeroes(s, pad = 8):
+    s = s.replace('0b', '')
+    size = len(s)
+    if size > pad:
+        return s[-pad:]
+
+    pad_size = pad - size
+    while pad_size > 0:
+        s = '0' + s
+        pad_size -= 1
+
+    return s
+
+def create_str_parts_array(string, parts):
+    return [string[i:i + parts] for i in range(0, len(string), parts)]
+
+def encode_char(char, to_binary = True):
+    encoded = char.encode()[0]
+    if to_binary:
+        encoded = pad_zeroes(bin(encoded))
+
+    return encoded
+
+def encode_text(text, to_binary = True):
+    encoded_text = ''
+    for char in text:
+        encoded_char = encode_char(char, to_binary)
+        encoded_text += encoded_char
+
+    return encoded_text
+
+def create_encoded_text_parts(text, parts = 2):
+    encoded_text = encode_text(text)
+    text_parts = create_str_parts_array(encoded_text, parts)
+    text_parts.reverse()
+    return text_parts
+
+def convert_image_data(image_data, interceptors = [], iterations=None):
+    iterations = len(image_data) if iterations is None else iterations
+    for i in range(0, iterations):
+        for interceptor in interceptors:
+            image_data[i] = tuple(map(interceptor, image_data[i]))
+
+    return image_data
+
+def get_diff(f1 = FILE, f2 = STEG_FILE):
+    with Image.open(f1) as im1:
+        with Image.open(f2) as im2:
+            image1_data, image2_data = list(map(lambda im: im.getdata(), [im1, im2]))
+
+            diff_factor = 0
+            for i in range(0, len(image1_data)):
+                r1, g1, b1 = image1_data[i]
+                r2, g2, b2 = image2_data[i]
+                diff_factor += pow(r1 - r2, 2) + pow(g1 - g2, 2) + pow(b1 - b2, 2)
+
+    return {'pixels': len(image1_data), 'diff': ceil(sqrt(diff_factor))}
+
+def encode_image(text, file = FILE, steganographic_file = STEG_FILE):
+    with Image.open(file) as im:
+        if not im.mode == 'RGB': im = im.convert('RGB')
+
+        image_data = list(im.getdata())
+        convert_image_data(image_data, [lambda x: pad_zeroes(bin(x))])
+
+        text_parts = create_encoded_text_parts(text)
+
+        while len(text_parts) > 0:
+            for i in range(0, len(image_data)):
+                image_data[i] = tuple(map(lambda x: (x[:6] + text_parts.pop()) if len(text_parts) > 0 else x, image_data[i]))
+
+        convert_image_data(image_data, [lambda x: int(x, 2)])
+        im.putdata(image_data)
+        im.save(steganographic_file)
+
+        return len(text)
+
+def decode_image(file = STEG_FILE, text_size = 100):
+    with Image.open(file) as im:
+        if not im.mode == 'RGB': im = im.convert('RGB')
+
+        image_data = list(im.getdata())
+        relevant_image_cells_count = ceil(text_size * 4 / 3) #2 least significant bits encoded for each rgb portion
+        text_bits_count = text_size * 8 #number of bits that represent the text
+        convert_image_data(image_data, [lambda x: pad_zeroes(bin(x), 2)], iterations=relevant_image_cells_count)
+
+        bit_string = ''
+        for i in range(0, relevant_image_cells_count):
+            bit_string += ''.join(image_data[i])
+
+        bit_string = bit_string[:text_bits_count] #normalize bit string to text bit size
+        bits_array = create_str_parts_array(bit_string, 8)
+        decoded_text_array = list(map(lambda x: chr(int(x, 2)), bits_array))
+
+        return ''.join(decoded_text_array)
+
+if __name__ == '__main__':
+    if len(argv) > 1:
+        arguments = argv[1:]
+        args_count = len(arguments)
+        if args_count == 1 and arguments[0] == '--diff': print(get_diff())
+        elif args_count == 2:
+            action, value = arguments
+            if action == '--encode': print(encode_image(value))
+            elif action == '--decode': print(decode_image(text_size=int(value)))
+    else:
+        print('Please provide arguments')
