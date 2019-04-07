b005ca5e8e233e9e8f5ae0948ed6742d5f13b0ea
Guy Peer <guy.peer@naturalint.com>
2019-04-07 18:51:45 +0300

    more modular code

--- steganography.py
+++ steganography.py
@@ -19,9 +19,11 @@ def is_encodable(text, image_data):
     required_image_data_size = get_required_pixels_for_text_encoding(len(text))
     return required_image_data_size < image_data_size
 
-def convert_image_data(image_data, interceptors = [], iterations=None):
-    iterations = len(image_data) if iterations is None else iterations
-    for i in range(0, iterations):
+def convert_image_data(image_data, interceptors = [], skip = 0, iterations = None):
+    image_size = len(image_data)
+    iterations = image_size if iterations is None else iterations
+    to = min(skip + iterations, image_size)
+    for i in range(skip, to):
         for interceptor in interceptors:
             image_data[i] = tuple(map(interceptor, image_data[i]))
 
@@ -35,15 +37,17 @@ def encode_image(text, file = FILE, steganographic_file = STEG_FILE):
         if not is_encodable(text, image_data):
             raise Exception(ENCODING_ERROR.format(len(text), len(image_data)))
 
-        convert_image_data(image_data, [lambda x: pad_zeroes(bin(x))])
-
         text_parts = create_encoded_text_parts(text)
+        convert_image_data(
+            image_data,
+            [
+                lambda x: pad_zeroes(bin(x)),
+                lambda x: (x[:6] + text_parts.pop()) if len(text_parts) > 0 else x,
+                lambda x: int(x, 2),
+            ],
+            iterations=get_required_pixels_for_text_encoding(len(text))
+        )
 
-        while len(text_parts) > 0:
-            for i in range(0, len(image_data)):
-                image_data[i] = tuple(map(lambda x: (x[:6] + text_parts.pop()) if len(text_parts) > 0 else x, image_data[i]))
-
-        convert_image_data(image_data, [lambda x: int(x, 2)])
         im.putdata(image_data)
         im.save(steganographic_file)
 
@@ -55,12 +59,15 @@ def decode_image(file = STEG_FILE, text_size = 100):
 
         image_data = list(im.getdata())
         relevant_image_cells_count = get_required_pixels_for_text_encoding(text_size)
-        text_bits_count = text_size * 8 # number of bits that represent the text
-        convert_image_data(image_data, [lambda x: pad_zeroes(bin(x), 2)], iterations=relevant_image_cells_count)
 
-        bit_string = ''
-        for i in range(0, relevant_image_cells_count):
-            bit_string += ''.join(image_data[i])
+        text_bits_count = text_size * 8 # number of bits that represent the text
+        convert_image_data(
+            image_data,
+            [lambda x: pad_zeroes(bin(x), 2)],
+            iterations=relevant_image_cells_count
+        )
+
+        bit_string = ''.join([''.join(image_data[i]) for i in range(0, relevant_image_cells_count)])
 
         bit_string = bit_string[:text_bits_count] # normalize bit string to text bit size
         bits_array = create_str_parts_array(bit_string, 8)
